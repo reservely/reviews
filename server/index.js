01b025f71b6612e6e5c8c2ff5fbfc9d14854bc7b
Janice Lam <janice.lam21@gmail.com>
2019-07-05 15:57:09 -0700

    modify server index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const seed = require('./db/seed.js');
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
 
+// when we start the server, seed.getrandinfo will fill our
+// database with 100 reviews, 100 users and 100 restaurants
 app.get('/', (req, res) => {
   seed.getrandinfo((err, data) => {
     if (err) {
