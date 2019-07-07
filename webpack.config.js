2f738a15e22483806fcba5c6c617091e25d6bd5d
Janice Lam <janice.lam21@gmail.com>
2019-07-06 18:59:04 -0700

    modify setup files

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
+const path = require('path');
+
 module.exports = {
-  entry: __dirname + '/client/src/index.jsx',
+  mode: 'development',
+  watch: 'true',
+  entry: './client/src/index.jsx',
   module: {
     rules: [
       {
-        test: [/\.jsx$/],
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
-        },
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/client/dist',
+    path: path.resolve(__dirname, '/client/dist'),
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
   },
 };
