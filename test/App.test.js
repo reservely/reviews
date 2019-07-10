4133bd5ebad1b0a0dd74caebde4a222a12fb4841
Janice Lam <janice.lam21@gmail.com>
2019-07-09 21:00:06 -0700

    writing tests for dropdown filter menu

--- test/App.test.js
+++ test/App.test.js
@@ -3,11 +3,10 @@ import render from 'react-test-renderer';
 
 import App from '../client/src/components/App';
 
-describe('App component', () => {
+describe('App Component', () => {
   it('renders reviews summary and individual reviews sections', () => {
     const component = render.create(<App />);
     const json = component.toJSON();
     expect(json.children[0].children).toHaveLength(2);
-    expect(json).toMatchSnapshot();
   });
 });
